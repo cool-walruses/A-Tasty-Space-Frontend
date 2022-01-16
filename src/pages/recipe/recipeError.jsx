2a39cebe41549fc393fca9cc94042393263e419f
Glitch (quill-vast-ear) <none>
2022-01-16 15:07:30 +0000

    7️⃣🏙️ Updated with Glitch

--- src/pages/recipe/recipeError.jsx
+++ src/pages/recipe/recipeError.jsx
@@ -1,10 +1,16 @@
 import React, { useContext } from "react";
 
+import "./recipeError/styles.sass";
 
 function RecipeError({ error }) {
-
   return (
-      <div>Error recipe code: {error}</div>
+    <div id="recipe-error">
+      <div class="text-content">
+        <h1>Error loading recipe!</h1>
+        <span>Please refresh the page to try again.</span>
+      </div>
+      <img src="https://cdn.glitch.global/d94bd104-e04e-4362-8c25-9302f465398d/CookyPop.png?v=1642344817482" />
+    </div>
   );
 }
 
