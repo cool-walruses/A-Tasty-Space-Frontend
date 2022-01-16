bd7247b655094c5a1e88f29d0f76e8ebded1c06e
Glitch (quill-vast-ear) <none>
2022-01-16 18:39:58 +0000

    🌷🍁 Updated with Glitch

--- src/constants/site.js
+++ src/constants/site.js
@@ -3,4 +3,4 @@ export const SITE_NAME = "A Tasty Space"
 export const handlePageTitle = (page) => {
   return `${page} - {$SITE_NAME}`;
 }
-export const BACKEND_URL = "";
+export const BACKEND_URL = "https://shielded-beyond-04883.herokuapp.com/api";
